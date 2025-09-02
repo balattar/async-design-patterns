6b6614ce29626ed9fa007ae25054852c262c86c7
Bader AlAttar <68021685+balattar@users.noreply.github.com>
2025-09-02 15:01:03 -0700

    feat(project 3 shutdown): Add graceful shutdown logic to http server (#2)

--- projects/03_shutdown/src/main.rs
+++ projects/03_shutdown/src/main.rs
@@ -1,36 +1,69 @@
 use std::{future::Future, pin::pin, time::Duration};
 
-use anyhow::anyhow;
 use hyper::{Request, Response, body::Incoming, service::service_fn};
 use hyper_util::rt::TokioIo;
 use tokio::net::{TcpListener, TcpStream};
+use tokio_util::{sync::CancellationToken, task::TaskTracker};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let server = tokio::spawn(http_server());
+    let token = CancellationToken::new();
+
+    let shutdown_token = token.clone();
+
+    tokio::spawn(async move {
+        let _gaurd = shutdown_token.clone().drop_guard();
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to listen for ctrl-c");
+    });
+
+    let server = tokio::spawn(http_server(token));
 
     server.await??;
 
     Ok(())
 }
 
-async fn http_server() -> anyhow::Result<()> {
+async fn http_server(token: CancellationToken) -> anyhow::Result<()> {
+    println!("starting http server");
+    let tracker = TaskTracker::new();
+
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    loop {
-        let (stream, _addr) = listener.accept().await?;
-        tokio::spawn(conn_handler(stream));
+    while let Some(Ok((stream, _addr))) = token.run_until_cancelled(listener.accept()).await {
+        tracker.spawn(conn_handler(stream, tracker.clone(), token.child_token()));
     }
+
+    println!("shutting down server, waiting for connections to finish");
+
+    tracker.close();
+    tracker.wait().await;
+
+    Ok(())
 }
 
-async fn conn_handler(stream: TcpStream) -> anyhow::Result<()> {
-    let builder = hyper_util::server::conn::auto::Builder::new(TaskExecutor {});
-    let conn = pin!(builder.serve_connection(
+async fn conn_handler(
+    stream: TcpStream,
+    tracker: TaskTracker,
+    token: CancellationToken,
+) -> anyhow::Result<()> {
+    println!("handling connection");
+    let builder = hyper_util::server::conn::auto::Builder::new(TaskExecutor(tracker));
+    let mut conn = pin!(builder.serve_connection(
         TokioIo::new(stream),
         service_fn(|req| async { req_handler(req).await }),
     ));
 
-    conn.await.map_err(|e| anyhow!(e))
+    match token.run_until_cancelled(conn.as_mut()).await {
+        Some(res) => return res.map_err(|e| anyhow::anyhow!(e)),
+        None => {
+            println!("connection cancelled; Completing any outstanding requests");
+            conn.as_mut().graceful_shutdown();
+            let res = conn.await;
+            return res.map_err(|e| anyhow::anyhow!(e));
+        }
+    }
 }
 
 async fn req_handler(req: Request<Incoming>) -> anyhow::Result<Response<String>> {
@@ -42,7 +75,7 @@ async fn req_handler(req: Request<Incoming>) -> anyhow::Result<Response<String>>
 }
 
 #[derive(Clone)]
-struct TaskExecutor {}
+struct TaskExecutor(TaskTracker);
 
 impl<Fut> hyper::rt::Executor<Fut> for TaskExecutor
 where
@@ -50,6 +83,7 @@ where
     Fut::Output: Send,
 {
     fn execute(&self, fut: Fut) {
-        tokio::spawn(fut);
+        // This isn't really necessary (i.e. using the task tracker); hyper tracks internally.
+        self.0.spawn(fut);
     }
 }
